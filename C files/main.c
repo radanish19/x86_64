2891601024790307cacfc071f848f7a61760d512
radanish19 <57444687+radanish19@users.noreply.github.com>
2021-04-20 16:12:07 +0500

    Create main.c

---
+++ C files/main.c
@@ -0,0 +1,10 @@
+#include "print.h"
+
+void kernel_main() {
+   
+    print_clear();
+    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
+    print_str(" ____  _____ _____ ____ ____ \n / ___|| ____| ____/ ___/ ___| \n\___ \|  _| |  _|| |  \___ \n ___) | |___| |__| |___ ___) |\n|____/|_____|_____\____|____/  ");
+
+    
+}
